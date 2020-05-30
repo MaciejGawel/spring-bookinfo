10abe53f62fa2539de8ebd996fa0ef5fbc62bdd3
Maciej Gawel <m.gawel.it@gmail.com>
2020-05-30 19:43:41 +0200

    Initialize bookinfo app with Spring Boot
    Signed-off-by: Maciej Gawel <m.gawel.it@gmail.com>

---
+++ src/test/java/pl/edu/agh/bookinfo/BookinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.bookinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
